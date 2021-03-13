6e23255f589b797df044d1f5720c883f4fd21756
Sravan <sravanch.svu2015@gmail.com>
2021-03-14 00:47:39 +0530

    Added Logs

--- src/main/java/Listeners/ListenerAction.java
+++ src/main/java/Listeners/ListenerAction.java
@@ -3,6 +3,8 @@ package Listeners;
 import CustomizedAnnotations.FrameworkAnnotation;
 import ExtentReports.ExtentLogger;
 import ExtentReports.Reports;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.ISuite;
 import org.testng.ISuiteListener;
 import org.testng.ITestListener;
@@ -12,9 +14,11 @@ import java.io.IOException;
 
 public class ListenerAction implements ITestListener, ISuiteListener {
 
+    private static final Logger log = LogManager.getLogger(ListenerAction.class.getName());
 
     @Override
     public void onStart(ISuite suite) {
+        log.info("Initializing Extent Reports");
         Reports.initreports();
 
     }
@@ -23,13 +27,13 @@ public class ListenerAction implements ITestListener, ISuiteListener {
         try {
             Reports.flushReports();
         } catch (IOException e) {
-            System.out.println("Exception occured at : "+e);
+            log.error(e.getMessage());
         }
 
     }
     @Override
     public void onTestFailure(ITestResult result) {
-
+        log.error(result.getMethod().getMethodName() + " is Failed");
         ExtentLogger.fail(result.getMethod().getMethodName() + " is Failed",true);
 
 
@@ -37,11 +41,13 @@ public class ListenerAction implements ITestListener, ISuiteListener {
 
     @Override
     public void onTestSkipped(ITestResult result) {
+        log.error(result.getMethod().getMethodName() + " is Skipped");
         ExtentLogger.skip(result.getMethod().getMethodName() + "is Skipped",true);
     }
 
     @Override
     public void onTestStart(ITestResult result) {
+        log.info("Test Case Executing is :: " + result.getMethod().getMethodName());
         Reports.createTest(result.getMethod().getMethodName());
         ExtentLogger.info("Test Case Executing is :: " + result.getMethod().getMethodName());
 
@@ -53,6 +59,7 @@ public class ListenerAction implements ITestListener, ISuiteListener {
     @Override
     public void onTestSuccess(ITestResult result) {
         ExtentLogger.pass(result.getMethod().getMethodName() + "is Passed",true);
+        log.info(result.getMethod().getMethodName() + "is Passed");
 
     }
     @Override
