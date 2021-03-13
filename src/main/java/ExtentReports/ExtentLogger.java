c7d242eedbebad8a96f7b8b02bdef8c6023f5f66
Sravan <sravanch.svu2015@gmail.com>
2021-03-14 01:41:09 +0530

    Minor Fix

--- src/main/java/ExtentReports/ExtentLogger.java
+++ src/main/java/ExtentReports/ExtentLogger.java
@@ -2,6 +2,7 @@ package ExtentReports;
 
 import Drivers.DriverManager;
 import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.Status;
 import com.qa.Utils.*;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -16,24 +17,24 @@ public final class ExtentLogger {
 
     public static void pass(String message) {
 
-        ExtentReportManager.getExtentTest().pass(message, MediaEntityBuilder.createScreenCaptureFromBase64String(ScreenShotUtils.getBase64Img()).build());
+        ExtentReportManager.getExtentTest().log(Status.PASS, message, MediaEntityBuilder.createScreenCaptureFromBase64String(ScreenShotUtils.getBase64Img()).build());
 
     }
 
 
     public static void fail(String message,Throwable throwable) {
 
-        ExtentReportManager.getExtentTest().fail(message);
-        ExtentReportManager.getExtentTest().fail(throwable, MediaEntityBuilder.createScreenCaptureFromBase64String(ScreenShotUtils.getBase64Img()).build());
+        ExtentReportManager.getExtentTest().log(Status.FAIL,message);
+        ExtentReportManager.getExtentTest().log(Status.FAIL, throwable, MediaEntityBuilder.createScreenCaptureFromBase64String(ScreenShotUtils.getBase64Img()).build());
 
     }
 
     public static void skip(String message) {
-        ExtentReportManager.getExtentTest().skip(message, MediaEntityBuilder.createScreenCaptureFromBase64String(getScreenShot()).build());
+        ExtentReportManager.getExtentTest().log(Status.SKIP, message, MediaEntityBuilder.createScreenCaptureFromBase64String(getScreenShot()).build());
     }
 
     public static void info(String message){
-        ExtentReportManager.getExtentTest().info(message);
+        ExtentReportManager.getExtentTest().log(Status.INFO, message);
     }
 
     private static String getScreenShot(){
