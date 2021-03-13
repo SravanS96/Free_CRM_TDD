f9578496811d4ec8dcea561b472e3cef37002745
Sravan <sravanch.svu2015@gmail.com>
2021-03-14 02:07:25 +0530

    Added more logs

--- src/main/java/ExtentReports/ExtentLogger.java
+++ src/main/java/ExtentReports/ExtentLogger.java
@@ -1,14 +1,17 @@
 package ExtentReports;
 
 import Drivers.DriverManager;
+import FreeCRM_Pages.Basepage;
 import com.aventstack.extentreports.MediaEntityBuilder;
 import com.aventstack.extentreports.Status;
 import com.qa.Utils.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
 public final class ExtentLogger {
-
+    private static final Logger log = LogManager.getLogger(Basepage.class.getName());
 
     private ExtentLogger() {
 
@@ -16,24 +19,26 @@ public final class ExtentLogger {
 
 
     public static void pass(String message) {
-
+        log.info(message);
         ExtentReportManager.getExtentTest().log(Status.PASS, message, MediaEntityBuilder.createScreenCaptureFromBase64String(ScreenShotUtils.getBase64Img()).build());
 
     }
 
 
     public static void fail(String message,Throwable throwable) {
-
+        log.error(message);
         ExtentReportManager.getExtentTest().log(Status.FAIL,message);
         ExtentReportManager.getExtentTest().log(Status.FAIL, throwable, MediaEntityBuilder.createScreenCaptureFromBase64String(ScreenShotUtils.getBase64Img()).build());
 
     }
 
     public static void skip(String message) {
+        log.info(message);
         ExtentReportManager.getExtentTest().log(Status.SKIP, message, MediaEntityBuilder.createScreenCaptureFromBase64String(getScreenShot()).build());
     }
 
     public static void info(String message){
+        log.info(message);
         ExtentReportManager.getExtentTest().log(Status.INFO, message);
     }
 
--- src/main/java/FreeCRM_Pages/Basepage.java
+++ src/main/java/FreeCRM_Pages/Basepage.java
@@ -11,16 +11,13 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
 public  class Basepage {
-    private static Logger log = LogManager.getLogger(Basepage.class.getName());
 
 
     protected static void sendKeys(By by, String keys){
-       // log.debug("Entering Keys Into WebElement::"+by);
 
       WebElement element= ExplicitWaitFactory.performExplicitWait(WaitStrategy.PRESENCE,by);
       element.clear();
@@ -28,17 +25,16 @@ public  class Basepage {
       ExtentLogger.pass(keys + " is Entered successfully. ");
     }
     protected static void click(By by){
-      //  log.debug("Clicking On WebElement ::"+by);
+
         WebElement element=ExplicitWaitFactory.performExplicitWait(WaitStrategy.CLICKABLE,by);
         element.click();
     }
 
 
     protected static String getText(By by){
-       // log.info("Text Is Captured From WebElement ::"+by);
+
         WebElement element = ExplicitWaitFactory.performExplicitWait(WaitStrategy.PRESENCE,by);
-        String text = element.getText();
-        return text;
+        return element.getText();
     }
 
     protected void moveToElement(By by){
@@ -57,9 +53,9 @@ public  class Basepage {
 
         List<String> list= new ArrayList<>();
       List<WebElement>webElements = ExplicitWaitFactory.performExplicitWaitForMultiplesElements(WaitStrategy.PRESENCE,by);
-        for (int i = 0; i <webElements.size() ; i++) {
-      //      log.debug("Getting WebElement Text ::"+by);
-            String name = webElements.get(i).getText();
+        for (WebElement webElement : webElements) {
+            //      log.debug("Getting WebElement Text ::"+by);
+            String name = webElement.getText();
             list.add(name);
         }
         return list;
@@ -73,10 +69,8 @@ public  class Basepage {
     protected void switchToSecondWindow(){
     //    log.debug("Switching To Child Window");
         Set<String> windows = DriverManager.getDriver().getWindowHandles();
-        Iterator<String> iterator = windows.iterator();
-        while (iterator.hasNext()){
-            String child_window=iterator.next();
-            if (getCurrentWindowHandle() != child_window){
+        for (String child_window : windows) {
+            if (!getCurrentWindowHandle().equals(child_window)) {
                 DriverManager.getDriver().switchTo().window(child_window);
             }
         }
